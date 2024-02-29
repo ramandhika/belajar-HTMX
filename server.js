822f7f5435d2b6187bec1eb44b9c05f92e4d6c8b
Ramandhika <ramandhika.ilham@gmail.com>
2024-03-01 02:10:55 +0700

    Add loading indicator to button click with hx-indicator

--- server.js
+++ server.js
@@ -12,14 +12,16 @@ app.use(express.json());
 
 // Define your routes here
 app.get('/users', async (req, res) => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await response.json();
-    res.send(`
-    <h1 class="text-2xl font-bold my-4">Users</h1>
-    <ul>
-      ${data.map((user) => `<li>${user.name}</li>`).join('')}
-    </ul>
-  `);
+    setTimeout(async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        res.send(`
+        <h1 class="text-2xl font-bold my-4">Users</h1>
+        <ul>
+          ${data.map((user) => `<li>${user.name}</li>`).join('')}
+        </ul>
+      `);
+    }, 2000);
 });
 
 app.listen(port, () => {
